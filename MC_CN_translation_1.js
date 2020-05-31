51d2455807ca73b20ed3278ee71a8eaee325c763
Euxcbsks <56812006+Euxcbsks@users.noreply.github.com>
2020-06-01 04:06:35 +0800

    Create MC_CN_translation_1.js

---
+++ MC_CN_translation_1.js
@@ -0,0 +1,499 @@
+		"zh": {
+            "app_name":    		"My Colony",
+            "new-game-message":         "報告指揮官，我們剛登上這顆星球，並準備好開始殖民了。我們該叫它什麼名字？",
+            "welcome-message":          "您剛與#motherland簽下殖民合約，並來到這個專屬於您的殖民地，您可以在這做任何您想做的事，但首先您需要收集一些基本資源。  請點擊探勘車(Rover)，右下方會出現功能選單，請點擊黃色圖標讓探勘車開始收集資源。",
+            "rover-description":        "探勘車是遊戲初期最重要的工作單位。它能採集原礦和建造數種基礎設施。",
+            "lander-description":       "著陸器是遊戲初期最重要的建築之一，它提供2單位的電力與少量基本資源儲存空間，可切換至飛行模式以移動位置",
+            "mine-description":         "人造的原礦礦場。可以用探勘車或原礦運輸車來採集資源。  在遊戲初期，若您不想因原礦消耗殆盡而迎來Bad End的話，請多造一些原礦礦場",
+            "pavement-description":     "用道路取代殖民地崎嶇不平的表面，提升地面工作單位的移動速度。  若您有強迫症，也許它能滿足你。  [可升級至強化道路]",
+            "greenhouse-description":   "溫室也是遊戲初期最重要的建築之一，它能生產少量食物並提供少量食物儲存空間。  若您需要更多食物儲存空間，可以建造一些糧倉"
+            "silo-description":         "糧倉，就只是個糧倉，就只是個大桶子，能提供更多食物儲存空間。  [可升級至大糧倉]",
+            "atmosgen-description":     "小型大氣產生機，在一些大氣濃度較低的殖民地或許會看到它。  過低的大氣濃度可能導致殖民者們無法正常生存！",
+            "factory-description":      "一間小車廠，無數臺車輛！  平時會生產一些輪胎，用來製造更多工作車輛。",
+            "builder-description":      "基礎建築車在遊戲初期是相當常用的工作單位，大多數基礎建築由它們建造。  若想追求更高的建築速度，可以多造幾臺並將其合併為一個單位。",
+            "beta-info":                "My Colony 是一款測試中的殖民地建設模擬器及策略遊戲。  雖然內容不算豐富，但希望各位能夠玩的盡興，並在我們的官方論壇回報可能的任何問題，或提議新增新的文明、星球類型、建築、工作單位……等，讓這個遊戲能夠變得更完整！  也許某天你就會看到你提議的東西出現在遊戲裡！",
+            "lander-sell-warning":      "警告！  這則訊息不是教學的一部分！！  著陸器是遊戲初期唯一能夠儲存資源的建築，且無法透過任何方式建造！  除非您非常確定已經不需要它，否則不建議賣掉著陸器！！  您確定要將它賣掉？",
+            "refinery-description":     "基礎煉鋼廠，使用6單位電力並消耗原礦來生產鋼。  [可升級至進階煉鋼廠][無法移動的建築]",
+            "miner-description":        "原礦運輸車是能採集並運輸原礦的工作車輛，每臺/次可以運輸",
+
+            "small-panel-description":  "太陽能板能為殖民地產生少量的電力，且不需要輸入任何資源。",
+
+            "landing-pad-description":  "新的殖民者每隔一段時間就會透過著陸點來到你的殖民地。",
+
+            "small-shelter-description":"最基本的小型房屋。最多可住3個殖民者。",
+
+            "chop-shop-description":    "拆車場能回收一些老舊、多餘、或沒有用的工作車輛(尤其是探勘車)。",
+
+            "goldminer-description":    "黃金採集者(Gold Miner)能開採黃金並運輸至黃金精煉場。",
+
+            "goldrefinery-description": "黃金精煉廠能儲存金礦，並把它們精煉成純度更高的黄金。",
+
+            "goldsmelter-description":  "製幣廠能把黃金鑄造成閃閃發亮的硬幣！",
+
+            "hire-button-change-job":   "更改工作",
+
+            "hire-button-hire":         "聘用",
+
+            "object-sell":              "出售",
+
+            "object-remove":            "移除",
+
+            "side-menu-game-data":      "遊戲資訊",
+
+            "delete-game-confirm":      "確定刪除這個殖民地?",
+
+            "header-delete-colony":     "刪除殖民地",
+
+            "tax-window-header":        "#motherland 徵税",
+
+            "tax-window-content":       "鑒於你的殖民地相當成功， #motherland 大會很高興的通知您，您的殖民地需要向 #motherland 繳稅。",
+
+            "tax-window-owed-header":   "稅金: ",
+
+            "tax-window-pay":           "付稅",
+
+            "tax-window-reject":        "回絕",
+
+            "tax-window-pay-response":  " #motherland 大會感謝您的合作以及對聯邦的貢獻。",
+
+            "tax-window-reject-response": " #motherland 大會提醒您，依照聯邦法律的規定，您必須繳交稅款。大會稍後會再作通知。 ",
+
+            "adv-hydro-desc":           "學習種植水耕作物的進階技術。可以大幅提升食物的產量",
+
+            "hd-solar-desc":            "更好的太陽能科技。用相同的面積，產生更多的電力",
+
+            "smalllab-description":     "成立一個小型研究團隊，他們將在這裡進行無數的實驗。研究得到的研究點可以用來解鎖新的科技",
+
+            "advancedhydro-description":"進階水耕農場是一種能大量生產食物的高科技設施。需要工人來讓它保持運作。",
+
+            "solar-tower-description":  "將太陽能板以更高的密度搭在一起，我們建造出了太陽能塔，它能用相同的空間產生更多的電力。",
+
+            "adv-ore-pur-desc":         "更高效率的冶煉技術，用相同的時間冶煉更多的鋼",
+
+            "builder2-description":     "進階的基礎建築車，用來建造更多進階建築，有點饒舌？沒關係，用就對了(oωo)b",
+
+            "construction-2-desc":      "解鎖進階的建築方式，讓您能夠建造更多全新的建築和工作車輛。",
+
+            "pavement2-description":    "用鋼強化過的路面。雖然有點貴，但能讓地面車輛以更快的速度行駛。放心，不至於讓您的車輛快到翻車",
+
+            "silo2-description":        "更大的筒倉，更大的食物容量！越大，越好！",
+
+            "finance-theory-desc":      "研究如何在您的太空殖民地發展經濟。相信我，之後您會很需要錢的",
+
+            "refinery2-description":    "這間進階煉鋼廠比一般的煉鋼廠快5倍！多造幾間，然後讓您的鋼多到滿出來！需要工人來讓它保持運作。",
+
+            "adv-gold-desc":            "允許建造和挖堀黃金礦場。",
+
+            "goldmine-description":     "從地面挖出一個黃金礦場並獲取黃金。邏輯？別管它了",
+
+            "rescomplex-description":   "能住26個人的中型公寓。請在附近建造一些學校、娛樂設施或其他任何需要的建築，讓殖民者更樂意為您工作",
+
+            "greendome-description":    "圓項公園為殖民者提供免費的休閒娛樂，使他們不至於悶到發瘋。",
+
+            "massdriver-description":   "貨物發射器是一座強力的電磁動能發射器，它能將貨物發射到 #motherland並出售。需要工人來讓他保持運作；運作其間亦產生少量金錢。",
+
+            "lg-manufacturing-desc":    "學習如何在嚴竣的環境下生產各種商品供應殖民地的需求。",
+
+            "chipfactory-description":  "晶片工廠可將黃金加工成為晶片。26個工人每天為您生產少量的晶片。",
+
+            "tax-payment-thankyou":     "#motherland 大會感謝您的合作。",
+
+            "tax-payment-insufficient": "您繳交給 #motherland 的金額不夠付清稅款。欠款會累積到下次繳稅。",
+
+            "stage-1-intro":            "星球上的大氣濃度開始足以維持微生物的生命！",
+
+            "stage-2-intro":            "隨著星球上的大氣增加，小型植物開始生根發芽！",
+
+            "stage-3-intro":            "現在的大氣濃度與地球相似，所有人可以自由地在室外活動！",
+
+            "col-civics-desc":          "學習如何開始為你的人民成立殖民政府、建造公民服務設施，以及訂立法律！",
+
+            "atmosgen2-description":    "一部擁有更高效率、更高耗能的中型製氣機。",
+
+            "civiccenter-description":  "建立殖民政府由公民服務做起！公民中心消耗資金生產公民值，並需要工人運作。",
+
+            "statue1-description":      "獻給第一批踏上這片土地的勇士。",
+
+            "fracking-description":     "壓裂採礦站使用一種高科技、高耗能的方法開採礦石，過程中完全不需要使用礦石採集者。需要工人及大量電力運作。",
+
+            "adv-chem-desc":            "探索科學中把物質轉換的祕密 -- 進階化學。",
+
+            "gldsynth-description":     "點「礦」成金！這間小型的合成黃金實驗室可以將原礦轉化為黃金！需要工人以及大量的電力來保持運作。",
+
+            "la-zoology-desc":          "學習如何在低大氣濃度的環境培養野生動物。",
+
+            "hatchery-description":     "魚類養殖場為殖民地提供新鮮又美味的魚！Les Poissons！",
+
+            "icechipper-description":   "這部小型的破冰者(Ice Chipper)能從冰礦開採水源並將水運回殖民地。",
+
+            "waterbot-description":     "這部小型的集水機械人(Waterbot)能從冰礦和水源收集飲用水並將其運回殖民地。",
+
+            "atmoscondensor-description":"大氣凝水機從空氣收集含水份的大氣，並將其凝結成純飲用水。",
+
+            "pet-expansion-desc":       "透過從 #motherland 大會購買土地使用許可證以擴張你的殖民地。",
+
+            "pet-annex-desc":           "在沒有 #motherland 大會許可下吞併周邊土地。可能會觸發衝突。",
+
+            "smalltank-description":    "一個儲水缸，用來儲存和分配飲用水。",
+
+            "waterpump-description":    "從地下抽取並過濾小量飲用水的小型水泵。100%保證不會導致地層下陷或其他問題。",
+
+            "aluminumrefinery-description":"鋁礦精煉廠能建造鋁礦採集者，並收集和儲存鋁。",
+
+            "watertower-description":   "相比儲水缸，水塔能儲存更多的水來滿足殖民者的需要。",
+
+            "adv-microbio-desc":        "學習如何在低大氣濃度的環境下培養微生物。",
+
+            "new-game":                 "開始新遊戲",
+
+            "new-game-col-name":        "殖民地名稱",
+
+            "custom-map-name":          "自訂地圖名稱",
+
+            "map-size-header":          "地圖尺寸",
+
+            "map-size-small":           "小型",
+
+            "map-size-medium":          "中型",
+
+            "map-size-large":           "大型",
+
+            "resources-header":         "資源",
+
+            "resources-scarce":         "稀少",
+
+            "resources-normal":         "一般",
+
+            "resources-abundant":       "豐富",
+
+            "nuke-physics-desc":        "了解如何在低大氣濃度環境下開採和提取鈾作核能發電用途。",
+
+            "uraniumminer-description": "鈾礦採集者(Uranium Miner)能開採鈾礦，並將這些核幅射物料送到核分裂發電廠。",
+
+            "nuclearplant-description": "利用鈾的核分裂產生的熱，蒸發水來推動渦輪機，並產生大量的電力！只要你給他足夠的水，理論上不會過熱爆炸。但是，誰知道呢，「或許」會發生什麼喔！",
+
+            "bacteriafarm-description": "微生物農場透過培養微生物生產大量的大氣。",
+
+            "greenbot-description":     "綠化機械人(Greenbot)是一部能構建農業設施和綠化裝飾的建築機械人。",
+
+            "firtrees-description":     "這些常綠杉樹能在低大氣密度下生長。可以作為裝飾，亦可採收作為木材。",
+
+            "engine-keyboard":          "鍵盤控制設定",
+
+            "engine-keyboard-meta":     "為遊戲設定快捷鍵。",
+
+            "general-cancel":           "取消",
+
+            "keyboard-clear-menus":     "清除活躍對話",
+
+            "keyboard-idle-unit":       "尋找空閒單位",
+
+            "keyboard-sell-noconf":     "直接出售建築 (無須確認)",
+
+            "hold-and-click-tip":       "長按按鍵組合以選擇建築",
+
+            "keyboard-upgrade-noconf":  "直接升級建築 (無須確認)",
+
+            "when-object-selected":     "在物件被選擇時可用",
+
+            "keyboard-move-unit":       "移動已選單位",
+
+            "keyboard-harvest-mode":    "啟動資源採集",
+
+            "keyboard-show-build":      "顯示可建造選項",
+
+            "keyboard-show-export":     "顯示貨物出口選項",
+
+            "keyboard-show-import":     "顯示貨物進口選項",
+
+            "keyboard-sell":            "出售已選物件",
+
+            "keyboard-upgrade":         "升級已選物件",
+
+            "keyboard-defaults":        "回復預設設定",
+
+            "keyboard-defaults-warn":   "這將會清除之前的自訂設定！",
+
+            "keyboard-cancel-nocon":    "直接取消建造 (無須確認)",
+
+            "keyboard-cancel":          "取消建造",
+
+            "general-move":             "移動",
+
+            "general-harvest":          "採集資源",
+
+            "general-build":            "建造",
+
+            "general-upgrade":          "升級",
+
+            "general-import":           "貨物進口",
+
+            "general-export":           "貨物出口",
+
+            "general-dismiss":          "關閉",
+
+            "medicalclinic-description":"診所可為殖民者提供醫療服務。",
+
+            "general-park-unit":        "空閒時停泊位置",
+
+            "general-park-confirm":     "停泊位置已設定。",
+
+            "engine-sidebar":           "側欄界面切換",
+
+            "tax-window-trade-embargo": "由於多次拖欠 #motherland 税務， #motherland 大會決定向您的殖民地實施禁運。",
+
+            "tax-window-lift-embargo":  "#motherland 大會決定移除對您的殖民地實施的禁運。",
+
+            "tax-window-img-embargo":   "由於您的殖民地與 #motherland 關係惡化， #motherland 的大會決定向您的殖民地實施全面移民封鎖令。",
+
+            "tax-window-img-lift":      "#motherland 大會決定暫時重新開放對您的殖民地的移民。 ",
+
+            "wall-description":         "牆壁可以用來装飾，亦可用作限制地面交通的路障。",
+
+            "lumbermill-description":   "一個鋸木廠，純粹將收集來的原木加工成木材。",
+
+            "lumberbot-description":    "伐木機械人(Lumberbot)可以砍伐和種植樹木。",
+
+            "sfhouse-description":      "帶好您的妻兒，一起入住這間懷舊的家庭式木屋！",
+
+            "megabot-description":      "這部又大又耐用的巨型建築機械人能構建更大型的建築。",
+
+            "urenrich-description":     "為您呈現核子科學和工程學的精彩傑作，可以將鈾的效能提昇一倍的鈾增值設施！需要大量電力和穩定的鋁供應。",
+
+            "lgwatertower-description": "一座能儲存大量飲用水的巨型水塔，就是這麼簡單。",
+
+            "largeres-description":     "一座高密度的大型住宅大樓。並不是很豪華，但能容下多達320個居民。",
+
+            "indepence-noafford":       "您未有足夠能力向 #motherland 宣示獨立。",
+
+            "independence-general":     "爭取獨立",
+
+            "general-united-earth":     "地球聯邦",
+
+            "relations-header":         "與 #motherland 關係",
+
+            "foundation-header":        "成立: ",
+
+            "foundation-charter":       "透過與 #motherland 簽訂的的殖民地憲章，在",
+
+            "independence-ask":         "您確認要以 #cost 向 #motherland 宣佈獨立？",
+
+            "general-nevermind":        "沒關係",
+
+            "independence-declare":     "宣佈獨立！",
+
+            "independence-final":       "我們在 #independenceday 成功向 #motherland 宣佈獨立！",
+
+            "independence-stat":        "從 #motherland 宣佈獨立，在",
+
+            "selfgov-desc":             "學習如何運行一個自給自足的獨立國家政府。",
+
+            "capitol-description":      "為您的新國家建立一座象徵光榮的國會大樓！",
+
+            "subtfood-description":     "一座擁有環境控制設備的地下食物存儲設施，能夠儲存非常大量的食物。",
+
+            "empire-general":           "殖民聯邦",
+
+            "engine-zoom-buttons":      "顯示縮放按鈕",
+
+            "lgcons-desc":              "研究如何在低重力環境下建造很大的設施！",
+
+            "treefarm-description":	"植樹場種植樹木，並將其採收作為木材。",
+
+            "aluminumminer-description":"鋁礦採集者(Aluminium Miner)不僅個性友好，他們的拿手絕活是開採鋁礦。",
+
+            "new-game-charter":         "殖民憲章代碼 (可選)",
+
+            "charter-warning":          "使用憲章代碼在特定聯邦開始新遊戲將可能出現其他玩家製造的內容，包括讓人反感的資料，而這些內容未必被 Ape Apps 審查。您確定使用這個憲章代碼開始新遊戲？",
+
+            "general-charter":          "殖民憲章",
+
+            "general-yes":              "是",
+
+            "general-no":               "否",
+
+            "charter-info":             "您可以與朋友分享您的殖民憲章代碼，讓他們使用代碼在你的聯邦作為成員開始新遊戲。你可以從成員收稅，亦可以與成員交易。你的憲章代碼是 #charterid 。",
+
+            "charter-share":            "一起玩My Colony ，加入我的聯邦！ #charterid #appurl",
+
+            "general-share":            "分享",
+
+            "charter-settings-desc":    "廣佈您的殖民憲章，擴張您的殖民聯邦！",
+
+            "tax-collection-message":   "您收到了來自成員的最新繳稅！",
+
+            "bank-description":         "一家銀行，您的殖民者會在這裡存款和理財。生產資金和公民值。",
+
+            "gdp-general":              "本地生產總值 (GDP)",
+
+            "gdp-description":          "標示殖民地的經濟規模。",
+
+            "general-trade":            "銀河交易所",
+
+            "general-buy":              "購入",
+
+            "general-sell":             "出售",
+
+            "no-contracts":             "這裡暫時還沒有張貼交易合約哦！ ",
+
+            "select-resource":          "選擇資源",
+
+            "available-contracts":      "可交易的合同",
+
+            "multiples-notice":         "必須為100的倍數。",
+
+            "sell-amount":              "出售量:",
+
+            "buy-amount":               "購入量:",
+
+            "sell-price":               "每100單位價格:",
+
+            "boardoftrade-description": "貨易委員會大樓允許您在銀河交易市場內買賣資源！交易所需要網絡連線。",
+
+            "lrc-desc":                 "研究如何即時與其他星球的殖民地溝通。",
+
+            "galcom-desc":              "利用銀河系市場上的經濟潛力發展更大的殖民地！",
+
+            "pyramideye-description":   "這個巨型又神祕的金字塔能提供大量的免費娛樂！如果一千年後，考古學家會認為它是甚麼？星際帝國的古墓？神壇？還是古老的異星發電廠？都不是，這其實只是一個搶眼的旅遊景點！",
+
+            "stage-4-intro":            "您的人民開始抱怨因大氣污染形成的煙霧。",
+
+            "general-multiple":         "多個已選擇單位",
+
+            "deepexc-desc":             "學習如何挖一個深不見底的坑洞。",
+
+            "digsite-description":      "地表之下所藏何物？開一個大洞尋找它們吧！",
+
+            "colonial-website":         "殖民地專屬網頁",
+
+            "rename-colony":            "重新命名",
+
+            "rename-desc":              "不滿意殖民地現在的名字嗎？來更換一個吧！",
+
+            "aal-desc":                 "探索這些異星神器背後的祕密，研究指出它們擁有着非常強大的潛力！",
+
+            "aluminumgenerator-description":"重現古代外星人將鋼變成鋁的祕方！這一部非常昴貴的製鋁機會需要大量能量運作，同時亦需要穩定的水供應冷卻以避免過熱。",
+
+            "etbuilder-description":    "沒有人知道這些強大的異星建築機械人(Extraterrestrial Builder)是如何運作的，唯一已知的是它們是用已解碼的古代指令創造出來。能夠構建異星科技類設施。",
+
+            "giorgioinstitute-description":"有的時候你可能有這樣的問題 - 有沒有研究設施能提供研究的同時亦能提供異星神器的儲存。這個異星研究部門就是答案。",
+
+            "cloud-save":               "雲端同步存檔",
+
+            "cloud-save-description":   "當你已經登入Facebook, 你可以將遊戲存檔上雲端。這在多個裝置上同時遊玩同一個遊戲是一個很好用的功能，但是使用行動數據或過慢的網絡裝置可能有存檔錯誤，甚至檔案損毀的風險。",
+
+            "consulate-description":    "殖民領事館是 #motherland 與您的殖民地之間的管理樞紐，這裡可以讓您管理殖民地與所屬殖民聯邦的一切事務。亦是訂立殖民地政策的基礎設施。",
+
+            "new-game-random-charter":  "隨機聯邦",
+
+            "general-delete":           "刪除",
+
+            "distillery-description":   "水加食物，所生何物？答曰，此乃郎姆酒是也！",
+
+            "brewing-desc":             "學習如何在低重力環境下釀造酒精飲品。",
+
+            "rumcellar-description":    "一座地窖，能在穩定的環境控制下儲存郎姆酒。",
+
+            "general-approval":         "施政評價",
+
+            "relaybooth-description":   "網絡中繼站提供殖民者免費的娛樂，它連接 #motherland 的網絡，提供可愛貓貓的影片 (^•ω•^) 。同時也提供小量基本教育提升殖民者的IQ，從而讓他們能擔當更好的職業。",
+
+            "general-policy":           "政策",
+
+            "general-payroll":          "工作薪金",
+
+            "payroll-notice":           "該時候支付薪金啦！這次需支付的總額是 #payrollcost.",
+
+            "payroll-motherland":       "由於您是殖民聯邦 #motherland 的成員， #motherland 將會根據薪金支援比例資助 #motheroffset 。",
+
+            "payroll-reject":           "您決定了不給薪金。",
+
+            "payroll-insufficient":     "根據您的資金餘額，您所支付的薪金不足以供給所有人。一部分人可能會挨餓。",
+
+            "payroll-motherland-collect":"來自 #motherland 的薪金支援。",
+
+            "saving-dont-quit":         "存檔其間請勿退出遊戲 (•ω•)",
+
+            "tax-pay":                  "支付",
+
+            "general-sound-xf":         "音效",
+
+            "pub-description":          "轉郎姆酒為正能量！這家酒吧為殖民者提供一個能暫時將日常煩惱拋諸腦後的地方，一杯好酒只需一點點的費用。",
+
+            "general-destruct":         "自我毀滅",
+
+            "general-labor":            "工資政策",
+
+            "labor-desc":               "調整各項職業的薪金。",
+
+            "general-contine":          "繼續",
+
+            "general-new":              "開始新遊戲",
+
+            "general-load":             "載入存檔",
+
+            "general-options":          "更多設定",
+
+            "general-execute":          "處決",
+
+            "execute-confirm":          "您確定要將這個人處決(並將其財物充公)？這會招來人民反感。",
+
+            "general-destroy":          "摧毀",
+
+            "decfirtrees-description":  "如果想種樹裝飾一下又不想被伐木機械人砍掉，這個種在樹圃裡的杉樹一定合你需要。",
+
+            "cloning-desc":             "在未來，人類未必會以正常的方法繁殖。這時複製人技術就派上用場。",
+
+            "cloningfacility-description":"一個高科技複製人設施，新的殖民者會從實驗室培養出來。",
+
+            "claymine-description":     "在擁有充足大氣密度的情況下，你可以將水與土壤結合成黏土。這個黏土礦提供燒製陶器和磚塊所需的黏土。",
+
+            "clayprod-desc":            "結合化學、水耕技術和微生物學，研究如何使用貧瘠的土壤製造黏土。",
+
+            "general-activate":         "啟動",
+
+            "general-deactivate":       "關閉",
+
+            "general-activate-all":     "全部啟動",
+
+            "general-deactivate-all":   "全部關閉",
+
+            "claybot-description":      " 黏土機械人(Claybot)能挖堀黏土礦和收集黏土。",
+
+            "potteryworks-description": "將黏土加工成優質陶器！製瓷廠需要穩定的水供應。",
+
+            "warehouse-description":    "提供各類商品儲存空間的工業倉庫。",
+
+            "subhousing-description":   "一些人喜歡住在地下。這個配備室內環境調節器的地下住宅群可以容下大量居民！",
+
+            "chipfactory2-description": "進階微蕊片工廠能更快生產微蕊片並提供更多微蕊片存儲！",
+
+            "chipfactory3-description": "利用古代異星科技建造的超高效率微蕊片工廠！這座古代異星微蕊片工廠生產其間消耗異星神器，並需要穩定的水供應來冷卻。",
+
+            "aam-desc":                 "發現古代異星一族製造工業產品的祕方。",
+
+            "masonry-desc":             "學習如何在低重力環境下燒製磚塊。 ",
+
+            "brickroad-description":    "紅磚路是裝飾殖民地不錯的選擇。此外也能讓地面車輛行駛比在一般路面上更快",
+
+            "brickfactory-description": "將黏土加工成磚塊的製磚廠，所產出的磚塊可以用來蓋更多種類的建築。需要穩定水供應。",
+
+            "ybrickroad-description":   "當紅磚路遇上黄金，能蓋出閃閃發亮的光榮之路！黃磚路能讓你的殖民地看起來獨當一面！",
+
+            "warehouse2-description":   "紅磚工業倉庫能夠提供充足的工業儲存空間之餘，也很合適裝飾殖民地的工業區。",
+
+            "brickhouse-description":   "誰不想住在經典又舒適的红磚屋？",
+
+            "husbandry-desc":           "學習如何在低重力環境下飼養大型動物。",
+
+            "sheepfarm-description":    "綿羊農場飼養綿羊提供製布所需的羊毛。需要穩定的水供應。",
+
+            "textilemill-description":  " 織布廠把羊毛加工成軟綿綿又舒服的布料。",
+
+            "sweatshop-description":    "歡迎來到製衣血汗工廠，這裡沒有夢想，只有商業收益！這個工廠會把布料做成衣服，然後直接賣出去獲利；工資便宜，福利欠奉，包賺不用賠！",
+
+            "luxtower-description":     "豪華住宅大廈為上層社會人士提供一站式服務，從住所、工作崗位到娛樂通通都有。不過要滿足這些社會精英，您會需要供應郎姆酒。",
+
+            "aliencondensor-description":"古代異星一族能用這部異星大氣凝水裝置將很多的大氣變成水。我的意思是，非常多的大氣。留意它是否用得太多大氣，否則......",
+
+            "cst-desc":                 "最近的現象使97%的科學家認為這個星球會因為全球暖化而毀滅。尋找可用的解決方案。",
