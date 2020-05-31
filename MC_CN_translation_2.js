437e575152989525032e726c56c7ae5a2e886abb
Euxcbsks <56812006+Euxcbsks@users.noreply.github.com>
2020-06-01 04:12:02 +0800

    Create MC_CN_translation_2.js

---
+++ MC_CN_translation_2.js
@@ -0,0 +1,499 @@
+            "hydropower-description":   "如何將水化為大量電力？這個強力的氫反應爐就是例子！",
+
+            "largepark-description":    "這個大型公園會讓人回想起在 #motherland 的日子！提供大量免費娛樂。",
+
+            "waterpool-description":    "一塊方形的水池，用來裝飾最好不過。",
+
+            "lol-description":          "給全個銀河系知道你的殖民地有一個值得擁擠一下的景點--自由登陸像！",
+
+            "human-description":        "人類是地球的主要物種。當然你一定知道人類是甚麼。",
+
+            "red-planet-desc":          "紅色星球是典型的荒蕪星球。這個星球可能曾在千萬年前孕育生命，但現在只剩一片無邊荒原。擁有多種資源。",
+
+            "ue-description":           "地球是人類引以為傲的家鄉，亦是地球聯邦(United Earth)的總部所在。雖然其成立宗旨為促進和平穩定，但實際上卻用武力手段控制太陽系。",
+
+            "premium-notice":           "My Colony 是免費的應用程式，但部分內容僅限為 My Colony 提供資金支持的玩家，購買廣告移除即可解鎖多達 #premiumcount 種建築和工作車輛等的高級內容！ 大部分高級建築主要是裝飾性的，對遊戲影響不大；高級用家亦可以旋轉建築方向，也可以套用材質包到遊戲，更多功能陸續有來！未來將會新增更多高級用家專屬內容！您希望購買高級內容並享受零廣告待遇？",
+
+            "statue2-description":      "這個統治雕像像徵著您的權力已經遍佈這片大陸！",
+
+            "flowersred-description":   "用這個美麗的紅花花圃點綴你的殖民地！",
+
+            "goldenshelter-description":"當然，對於住在你那高尚又強大的殖民地的人來說，一般的小型房屋並不‘合適’他們。他們想要的是一座鍍金的房子。",
+
+            "general-offers":           "貿易合約",
+
+            "general-new-offer":        "提交貿易合約",
+
+            "hipsterbar-description":   "當你已經準備好享受從未聽過的最潮音樂、高得能刷爆你老爸信用卡的入場費，和價格比正常高七倍的超貴郎姆酒，你已經夠資格去喜皮士酒吧聚會啦！",
+
+            "alienquantum-desc":        "古代外星人對於量子力學十分精通。該輪到你去發現這個能帶動科技革命的技術啦！",
+
+            "warehouse3-description":   "利用量子科技的強大力量儲存貨物！這個量子工業倉庫雖然耗電大並且需要穩定水供應來冷卻壓縮機，但是能允許在狹小的空間儲存成千上萬的貨物。",
+
+            "quantumwater-description": "利用量子科技的強大力量儲存水！量子儲水缸能在有限空間儲存海量水源，需要用電，也需要水來冷卻壓縮器......但放心，這儲水缸的水是可以安全飲用的。",
+
+            "brickwall-description":    "一塊紅磚牆，或許比一般牆壁好看？",
+
+            "largeclinic-description":  "一間大型診所，能夠處理更多病人和拯救更多生命！",
+
+            "rpbase-desc":              "紅色星球地圖的基礎科技。",
+
+            "hbase-desc":               "人類種族的基礎科技。",
+
+            "uebase-desc":              "來自地球聯邦文明的基礎科技。",
+
+            "lunbase-desc":             "月球地圖的基礎科技。",
+
+            "lunar-planet-desc":        "一個月球，沒有生命，沒有兔子，沒有大氣層，並只有稀少的前期資源。開始時會稍有難度，但是能慢慢適應。",
+
+            "lunarrover-description":   "這個小小的月球探勘車(Lunar Rover)能收集灰壤和在月球建設基本設施！",
+
+            "heltank-description":      "用於儲存氦-3的小型儲氣缸。.",
+
+            "regpath-description":      "一個能讓工作車輛順暢行駛的灰壤碎石路。",
+
+            "waterextract-description": "用氦-3從壓碎的灰壤中提取冰粒，並將其加工成飲用水的飲用水提取機。",
+
+            "regcompact-description":   "灰壤壓縮機用機械方式將灰壤壓縮成礦石。",
+
+            "helextractor-description": "氦-3提取器可從灰壤提取出氦-3。",
+
+            "regmine-description":      "缺灰壤嗎？在這裡挖一個灰壤礦場開採吧！",
+
+            "fwbase-desc":              "森林星球地圖的基礎科技。",
+
+            "forest-planet-desc":       "準備好做微觀管理，因為這個地方基本上除了滿處都是樹就沒有多少可用資源。一塊相對困難的地圖，不建議新手去嘗試。",
+
+            "woodhut-description":      "提供2人住所的小木屋。不算是很舒適的住所，但是價格低廉且搞定基本住宿。",
+
+            "forestrover-description":  "不知道是怎樣做到的，科學家開發了一種只用木頭做的機動車 -- 林地探勘車(Forest Rover)。這科學嗎？這不重要，這只是個遊戲嘛。",
+
+            "waterwell-description":    "打井取水，就是這麼簡單。",
+
+            "potatopatch-description":  "一個馬鈴薯園圃，只需保持穩定水供應，就有美味的馬鈴薯(食物)供應啦。烤馬鈴薯誰不喜歡？",
+
+            "charcoalhut-description":  "木炭窯小屋把木材燒製成木炭。烏煙瘴氣的有點讓人討厭。",
+
+            "coalburn-description":     "木炭發電機透過燃燒木炭，以提供殖民地需要的電力。",
+
+            "coalcompact-description":  "當你用木炭壓縮機把木炭壓的實實的，它會變成各種工程所需的礦石。",
+
+            "advcp-desc":               "學習如何以更高效率生產木炭。",
+
+            "charfactory-description":  "木炭工廠以更現代、更高效率的方式生產木炭！",
+
+            "oilref-desc":              "讓‘黑色黃金’流動吧！學習如何在低重力環境下提取石油作各類工業用途。",
+
+            "mulchpath-description":    "用木屑鋪成的路面。",
+
+            "oiltank-description":      "一個儲油缸。",
+
+            "crudebot-description":     "採油機械人(Crudebot)，一天到黑開採黑石油採得全身黑！",
+
+            "flowerswhite-description": "一個白花花圃。這些花兒會讓你帶來微笑......除非你恨花。",
+
+            "flowersblue-description":  "沒辦法想到在浩瀚宇宙裡甚麼會比藍色花圃更適合點綴殖民地。",
+
+            "flowerspurple-description":"在一個自我尊重的殖民地，代表高貴的紫花是不可或缺的裝飾。",
+
+            "adv-chemsyn-desc":         "將化學昇華到全新的景界！以工業生產規模生産更複雜的化合物！",
+
+            "oilplant-description":     "燃燒石油發電！擁有最‘適度’的大氣排放。",
+
+            "grass-description":        "一片綠油油的草地。很適合用來製造綠化區，上方可以讓人走過和讓車駛過。",
+
+            "quantumfood-description":  "用以儲存極大量食物的量子糧倉。需要穩定水供應冷卻壓縮器。",
+
+            "pavement5-description":    "柏油路讓您的車輛以非常高速行駛！或許需要前銀
+
+往銀河交易所來齊集它所需的建造材料。",
+
+            "oilsynthlab-description":  "人造石油製造廠透過混合食物和木炭提煉石油。",
+
+            "plastfact-description":    "塑膠工廠能把骯髒髒的石油加工成乾淨平滑的塑膠！",
+
+            "sweatshop2-description":   "製造玩具的玩具血汗工廠。收到玩具的小孩固然開心，但在這個工廠裡的工人就不是這樣了。",
+
+            "livingwage-header":        "生活津貼",
+
+            "livingwage-desc":          "設定一個額外工資，無論有沒有工作都能收到津貼。",
+
+            "firedrill-header":         "消防演習",
+
+            "firedrill-desc":           "借消防演習把殖民者從家裡和工作場所撤離......並將一切住所和工作崗位重新分配。",
+
+            "work-policy":              "工時政策",
+
+            "work-policy-desc":         "設定殖民地的工時政策。",
+
+            "work-policy-normal":       "正常",
+
+            "work-policy-normal-desc":  "常規的工作政策。殖民者可以在需要時休息娛樂。",
+
+            "work-policy-light":        "輕鬆",
+
+            "work-policy-light-desc":   "減少工作時間，讓殖民者更多時間遊樂放鬆。",
+
+            "work-policy-hard":         "加班",
+
+            "work-policy-hard-desc":    "如果需要提升殖民地產量，加班是一個選擇。工人的休息時間會減少。",
+
+            "work-policy-extreme":      "極限加班",
+
+            "work-policy-extreme-desc": "當出現‘一些情況’時，你不得不以極端的24小時馬拉松式加班以大幅提升產量。可能導致死亡。",
+
+            "general-go-back":          "返回",
+
+            "general-go-back-desc":     "返回上一頁",
+
+            "money-shower-header":      "即時津貼",
+
+            "money-shower-desc":        "你的人民有不滿嗎？用錢解決問題！給所有人派發即時津貼。",
+
+            "tax-collection-header":    "一次性稅收",
+
+            "tax-collection-desc":      "當你需要一點錢的時候，使用這個政策進行一次性稅收。稅額為殖民者平均儲蓄25%的總和。",
+
+            "spaceport-description":    "太空港是旅客的出入中心。有極小機率出現非法移民。運作時消耗公民值。",
+
+            "coltour-desc":             "了解如何吸引星際旅客，並從他們身上賺錢。",
+
+            "motel-description":        "費用便宜的小旅館能給星際旅客在造訪你的殖民地的同時提供一個可以休息的落腳地。最基礎的旅遊業設施。",
+
+            "general-tourism":          "旅遊業",
+
+            "touress-desc":             "發現吸引和服務旅客的更佳方法。",
+
+            "hotel-description":        "一家能服務大量旅客的大酒店。由於旅客住酒店有帶走毛巾的習慣，您會需要提供穩定的布料供應。",
+
+            "smcasino-description":     "想要一些經典的賭博遊戲？在這個小型賭場應有盡有！吸引遊客和殖民者放鬆一下，並賺盡他們錢包裡的錢！也會自己生產小量金錢。",
+
+            "giftshop-description":     "在想製造出來的陶器該用在哪裡？甭想啦！都放在這個禮品店售給遊客吧！",
+
+            "tourtrap-desc":            "學會如何利用旅遊景點以銀河系規模發展旅遊業，並賺盡各地旅客的錢！",
+
+            "zoo-description":          "一個自我尊重的殖民地不可沒有動物園！為旅客和殖民者提供娛樂。運作時需要穩定的陶器供應。",
+
+            "bluehouse-description":    "這個藍色小屋將成為下一對新婚夫婦一生一世的美好回憶--這是一個組織新家庭的好地方。",
+
+            "pavement6-description":    "格子路面基本上與一般路面一樣，但是好看多了。",
+
+            "flowersyellow-description": "用這個漂亮的黃花花圃填補空地的空虛吧！",
+
+            "obelisk-description":      "慶祝和象徵力量與獨立的一座巨型紀念碑！",
+
+            "restaurant-description":   "讓旅客在這個充滿和諧氣氛的餐廳，坐在開揚的座位享受殖民地風味菜式！",
+
+            "resort-description":       "更大的渡假酒店，服務更多的旅客！需要穩定的布料供應。",
+
+            "mall-description":         "一個專門從旅客和殖民者身上賺大錢的大型室外購物中心！需要大量電力和工人，並且持續消耗布料和玩具。",
+
+            "general-private-charter":  "聯邦公開性設定",
+
+            "general-private-desc":     "決定您的殖民聯邦是否公開或私人。若果設定為公開，它則會在開新線上遊戲時在隨機殖民聯邦列表出現。",
+
+            "general-private":          "私人",
+
+            "general-public":           "公共",
+
+            "map-size-mega":            "巨型",
+
+            "map-size-xlarge":          "超大型",
+
+            "large-map-warning-title":  "大型地圖警告",
+
+            "large-map-warning-content":"請注意大型地圖會影響遊戲流暢度。您確定要以大型地圖開始新遊戲？",
+
+            "new-game-size-notice":    "留意大型地圖可能影響流暢度。",
+
+            "online-mode":              "線上模式",
+
+            "online-mode-desc":         "啟動線上模式允許您的殖民地進行網上貿易(銀河交易所)和加入殖民聯邦，並給予您的殖民地一個專屬網站。此遊戲會需要網絡連線。",
+
+            "offline-mode":             "離線模式",
+
+            "offline-mode-desc":        "轉換成離線模式將會失去網上貿易功能；您成立的的殖民聯邦亦會解散，所有旗下殖民地會直接取得獨立。您的殖民地將無需網絡連線，但亦可與在同一局域網的玩家互傳訊息和貿易。",
+
+            "offline-mode-warning":     "您確定轉換成離線模式嗎？所有線上功能，包括網上貿易、殖民聯邦和殖民地網站將會失去。取得獨立的旗下殖民地不會在下次轉回線上模式時成為聯邦成員。",
+
+            "general-notice":           "公告",
+
+            "blacklist-notice":         "您的殖民地因涉及在線上使用黑科技作弊列入黑名單，此殖民地永久禁止使用線上模式。在離線模式下不會禁止使用作弊；但為了保持線上模式的公平性，線上作弊的殖民地會被禁止參與線上模式。感謝您的合作。",
+
+            "museum-description":       "博物館能提升殖民地的文化地位！能夠吸引遊客的景點，運作期間消耗陶器。",
+
+            "resources-report":         "提供殖民地資源生產及消耗數據的資源報告",
+
+            "galfreight-description":   "銀河貨櫃站能進出口更大量和更多種類的貨物。這些飛行貨櫃需要消耗石油作為飛行燃料。",
+
+            "statue3-description":      "這是全部建築之中最昂貴的無忌揮霍者之像。向您的人民表現您的財富和力量。另一方面，人民會視它為榨壓的象徵。",
+
+            "udexc-desc":               "利用外星科技挖一個更大更深的巨坑！看看能找到甚麼？",
+
+            "digsite2-description":     "一個深不見底的超深堀挖站。生產更多的異星神器，運作期間消耗水並產出小量黏土作為副產品。",
+
+            "general-rename":           "重命名",
+
+            "rename-colonist":          "給 #colonist 取一個新名字！",
+
+            "keyboard-open-stats":      "開啟數據庫",
+
+            "keyboard-open-tech":       "開啟可研究列表",
+
+            "keyboard-open-encyclopedia":"開啟百科全書",
+
+            "keyboard-open-unemployment":"顯示無業殖民者",
+
+            "keyboard-park":            "停泊單位",
+
+            "keyboard-zoom-in":         "畫面放大",
+
+            "keyboard-zoom-out":        "畫面縮小",
+
+            "charcoalpile-description": "留一塊空地作木炭儲存，然後把木炭都丟去這個木炭堆。",
+
+            "alienpower-description":   "利用古代異星科技所建造的異星能量塔！透過使用氦-3製造超大量的電力！",
+
+            "autosave-freq":            "自動儲存設定",
+
+            "autosave-freq-desc":       "設定自動儲存的頻率。可能影響流暢度。",
+
+            "autosave-freq-often":      "經常",
+
+            "autosave-freq-normal":     "正常",
+
+            "autosave-freq-rare":       "很少",
+
+            "autosave-freq-never":      "從不",
+
+            "autosave-often-warn":      "高頻率自動儲存會影響遊戲流暢度。",
+
+            "autosave-never-warn":      "記得要自己存檔避免遊戲進度遺失！",
+
+            "general-send-custom-msg":  "傳送訊息",
+
+            "general-send-msg-warn":    "傳送一個最多120個字元的訊息。注意，凡是傳送含不恰當內容、威脅性及騷擾性的訊息的殖民地將會列入黑名單。",
+
+            "game-compression":         "存檔壓縮",
+
+            "game-compression-desc":    "存檔壓縮可減少存檔佔據的儲存空間，但會增長存檔時間。將其關閉可加快存檔，但也會關閉雲端同步功能。",
+
+            "colony-sharing":           "分享殖民地",
+
+            "colony-sharing-desc":      "使用雲端同步存檔的玩家可以生成一個網址讓其他玩家以唯讀模式檢視存檔。",
+
+            "general-upgrade-all":      "升級全部",
+
+            "general-ugall-desc":       "確定升級所有同類建築？此將消耗大量所需資源和時間。",
+
+            "pavement7-description":    "用玩具建造五彩繽紛的彩虹路！",
+
+            "mound-description":        "蟲塚是蟲族的重要繁殖及食物製造基本設施。",
+
+            "waterbug-description":     "為殖民地從各種來源收集水源並將其運回蟲巢的藍水蟲(Waterbug)。",
+
+            "workerdrone-description":  "工蟻(Worker Drone)能從甘蔗收集糖，並能建造基本設施。",
+
+            "insectoid-description":    "蟲族是原生於波湖蘭(Bouwland)的進化物種，有著極快的繁殖速度，消耗資源的速度比蝗蟲還快。",
+
+            "zolargprime-description":  "以古索拉格(Zolarg Prime)為中心的索拉格帝國(Zolarg Empire)是一個蟲族帝國，以快速繁殖和驚人的資源消耗量聞名銀河系。",
+
+            "sugarland-desc":           "甘蔗林星球因大氣密度合適及氣候溫暖而生滿青蔥的甘蔗，是很適合蟲族定居的星球。",
+
+            "orebug-description":       "礦工蟲(Orebug)擁有能夠粉碎堅石的下顎，派遣去開採礦石最合適不過了。",
+
+            "feastingarea-description": "盛宴之地是蟲族聚集共享美食之地，同時提供娛樂。這裡也將糖加工成食物並儲存它們。",
+
+            "watervats-description":    "公共水缸是蟲族喜歡聚會討論生活的地方。提供水儲存及娛樂。",
+
+            "pastetank-description":    "製糊缸以最原始的方式製造蟻糊質，一種混合水、糖、蟲族口水而成的強力黏合劑。",
+
+            "pastepath-description":    "用蟻糊質和碎石鋪成的路。蟲族喜歡在這種黏乎乎的黏糊路上走。",
+
+            "broodcenter-description":  "幼崽培訓中心訓練蟲族幼崽作為成年所需的技能，並從當中生產研究。",
+
+            "zgoldstorage-desc":        "學習如何開採黃金，並挖地洞作黄金儲存用途。",
+
+            "goldbug-description":      "這隻老練的金礦蟲(Goldbug)是一種懂得開採黃金的工蟲。",
+
+            "goldburrow-description":   "一個讓金礦蟲堆放和儲存黃金的儲金洞。",
+
+            "sugarpower-res-desc":      "發現糖力發電的祕密！",
+
+            "sugarpower-description":   "燃糖發電機透過燃燒糖來製造電力！",
+
+            "communialfin-desc":        "讓蟲族學習如何匯集資源及利用通用貨物做生意。",
+
+            "insectoidmint-description":"蟲族鑄幣場。將黃金製成金燦燦的金幣。",
+
+            "backupOption":             "後備存檔 (*.mcb)",
+
+            "backupOptionDesc":         "每次存檔都會多儲存一個mcb格式未經壓縮的後備存檔。佔用較多的儲存空間。",
+
+            "sugarcult-desc":           "學習如何自己種植甘蔗！",
+
+            "antanium-prod-desc":       "學習生產褐蟻合金的方法。褐蟻合金是一種用於建築的堅硬物料。",
+
+            "antaniumsmelter-description":"合金熔爐將礦石和黄金煉成褐蟻合金，是發展蟲族殖民地的關鍵！",
+
+            "eldersmound-description":  "長老之塚是蟲族長老討論重要事件的地方。生產公民值。",
+
+            "miningoperation-description":"工蟻開墾出來的蟲族礦場。生產穩定的礦石及黃金供應。",
+
+            "zolargstatue-description":	"一座精緻的索拉格大帝雕像。這個偉大的蟲族人曾經領導他的同胞對抗邪惡的半蜥人政權--艾爾法•德拉科尼亞(Alpha Draconians)，並解放第一批蟲族奴隸，創下索拉格帝國。",
+
+            "foodfortification-description":"食品強化工場使用蟻糊質將食物濃縮化並將其變得更富營養價值。可以作為盛宴區的升級，但不提供糖儲存及娛樂。",
+
+            "solgov-desc":              "研究如何將您的管治提升到星系規模，並成立星系級別的政府。",
+
+            "capitol2-description":     "更大的聯邦固然需要更大的國會大樓，這個星系級國會大樓生產更多公民值，能執行更多政策，並允許更大額度的資源貿易。",
+
+            "galgov-desc":              "將管治手段更上一層樓！研究如何管理更大規模的事務和成立銀河系級別的政府。",
+
+            "capitol3-description":     "以更豪華的銀河級國會大樓展示您的權力和為 #cityname 帶來光榮！允許更大額度的資源貿易及生產更多公民值，但您會需要朗姆酒滿足這些好酒成性的政治家。",
+
+            "consulate2-description":   "更大的星系級殖民領事館，允許更大額度的資源貿易及解鎖新政策。",
+
+            "consulate3-description":   "改良後的銀河級殖民領事館能進一步增加資源貿易上限及解鎖新政策。",
+
+            "sand-description":         "來些金光閃閃的沙子讓您的殖民地更顯燦爛。",
+
+            "capitol4-description":     "這座象徵愛國的民族國會大樓會使您的人民以成為 #cityname 的一分子為傲！產出更多的公民值並進一步增加資源貿易上限。",
+
+            "nationalism-desc":         "研究民族主義思想，並將其推廣到大眾以促進民族自豪感及人民的忠誠。",
+
+            "general-flip":             "旋轉",
+
+            "flag-description":         "以這面國旗宣傳我國的強盛和以國家為傲的自豪感！",
+
+            "capitol5-description":     "這個讓人又畏又敬的帝國國會大樓會告訴您的子民，成為極權帝國不再是值得懷疑的問題。生產更多的公民值及進一步增加資源貿易上限(以及更多政治家對朗姆酒的‘需要’)。",
+
+            "imperialism-desc":         "對於一些好權力的統治者而言，成為一個普通國家的統治者是不足夠的。只有成為帝國統治者，才能成為萬邦之上的霸主。研究如何推行帝國主義。",
+
+            "insregen-desc":            "發現蟲族醫藥學的精髓之處。",
+
+            "healingpods-description":  "蟲族的治療方式十分奇特，他們會將自己埋在混和草藥的泥土裡，然後稍等片刻......砰！完全回復！",
+
+            "rationing-header":         "緊急配給制",
+
+            "rationing-desc":           "在必要的時候你可以實施緊急配給制以減少糧水消耗，而消耗率是正常的1/3。注意此政策會導致健康風險。",
+
+            "general-native-client":    "本地用家",
+
+            "native-client-description":"此瀏覽器版本的My Colony未有本地檔案的存取權，因此無法在瀏覽器以外其他位置儲存遊戲存檔，並擁有相當高的存檔遺失風險。您可以選擇下載擁有本地存取憑證的應用程式版本，將存檔保存至本機檔案庫。需要查看可下載選項嗎？",
+
+            "sugarfarm-description":    "相信沒有人會選擇在野外去收集野生甘蔗，倒不如自己開墾甘蔗農田方便一下自己。",
+
+            "general-annex-land":       "吞併土地",
+
+            "general-annex-desc":       "利用自身的名聲和勢力吞併周邊土地。",
+
+            "gentelbugs-description":   "當這些雄蟲對他們的妻子說在加班，他們大多都跑去紳士蟲俱樂部喝一杯了。消耗郎姆酒，提供娛樂，以及是年輕史官搜羅各種老故事(和做兼職)的地方",
+
+            "lghe3-description":        "更大的氦-3儲存缸。",
+
+            "plaque-description":       "若果有值得紀念的地方，這座紀念牌匾就正適合標記它們。",
+
+            "general-set-text":         "設定文字",
+
+            "galimp-desc":              "學習如何運行一個更大(且更邪惡？)的帝國......我指的是，銀河大帝國。",
+
+            "capitol6-description":     "這座銀河大帝國國會大樓會向全宇宙展示您的強大力量和 #cityname 的繁榮。能增加非常多的資源貿易限額。與此同時，一天會消耗無數的郎姆酒。",
+
+            "tallconst-desc":           "到了某些時候，與其尋找更多土地蓋房子，不如把建築蓋得更高來增加空間。學習在低重力環境下建造高樓的技術。",
+
+            "elevator-description":     "通天電梯連接著軌道太空港，它允許更大批的貨物進出口，以及促進新移民入住。相比銀河貨櫃站，它消耗更少燃油，但使用更多電力。",
+
+            "advrobo-desc":             "學習製造進階機械人及開發進階人工智能的技術！或許它能帶來新的電子機械革命？",
+
+            "condrone-description":     "別瞧不起這小小工程無人機(Construction Drone)，它可是把高樓大廈蓋起來的幕後功臣呢。",
+
+            "robofactory-description":  "這座高科技的機械人工廠生產高科技產業所需的機械人及建設高級建築的無人機類工作單位。需要穩定的鋁及微蕊片供應。",
+
+            "advdig-desc":              "蟲族最擅長的技術就是打地洞。學習進階的挖洞技術以製造更深的地洞，並解鎖新的建築。",
+
+            "wetmound-description":     "這個潮濕之塚的原理很簡單，就像水井那樣，先向下挖尋找地下水源，然後蓋一個塚型結構的抽水裝置。需要喝水時只要靠近吸一口就搞定了。",
+
+            "moneypit-description":     "聚錢坑是蟲族分配及管理公共財富的機構。多餘的錢丟進去，需要用時則取出，依據每一隻蟲的能力去分配......生產資金及公民值。",
+
+            "exportcannon-description": "科技不算很發達的蟲族是如何實現星際長程貨物運輸？它們老早就有解決方案了，靠的是這一門簡單的出囗炮，它以比人類還低的成本有效地把貨物發射回 #motherland 作貿易。運作其間生產小量資金。",
+
+            "general-live-prices":      "即時報價",
+
+            "lumberyard-description":   "儲木場能做甚麼？儲存木材。提供工作。生產收益。還能說甚麼？這簡直是多功能設施。",
+
+            "general-repair":           "修理",
+
+            "general-condemn":          "譴責",
+
+            "lottery-header":           "彩票抽獎活動",
+
+            "lottery-message":          "向殖民者出售彩票。中獎的幸運兒將會改變一生！(同時在沒有導致更多不滿的情況下籌募一些資金)",
+
+            "lottery-instruction":      "一枚彩票定價多少？值得提醒的是，沒人願意買太昂貴的彩票哦！",
+
+            "lottery-fail":             "不好意思，這次的彩票看來無人問津......",
+
+            "regolithextractor-description":"灰壤開採站是一座採集地下灰壤的重型工業設施，不需要蓋一次性礦場即可有無盡供應。",
+
+            "horrorhouse-description":  "鬼屋是結伴夜間遊樂的好去處！吸引遊客及提供娛樂，每人都會有玩具帶回家！",
+
+            "general-bad-trade-notice": "您的經濟顧問想通知您這單貿易不是一單對我方有利的生意。確認成交？",
+
+            "general-bad-trade-header": "來自經濟顧問的勸告",
+
+            "general-declare-ind":      "宣佈獨立",
+
+            "general-declare-ind-desc": "終斷與 #motherland 的殖民地聯繫並靠自己創立自己的殖民聯邦。",
+
+            "galfi-desc":               "學習銀河金融學的精髓之處--從銀河各地賺取收益！",
+
+            "bank2-description":        "在這座銀河級投資銀行顧用無數的文員，然後看著他們如何從全宇宙獲得財富！",
+
+            "restower-description":     "能容納大量人口的廉價住宅大廈。",
+
+            "steelyard-description":    "存鋼場用來大量儲存各種重要的合金。運作期間生產少量收益。",
+
+            "subtcharcoal-description": "一旦遇上了了潮濕氣候，在戶外儲存木炭就不是一個好辦法了。這個地下木炭儲存設施使用氦-3保持木炭乾乾爽爽，並比木炭堆提供更多儲存量。",
+
+            "general-gift-giver":       "銀河大帝",
+
+            "general-gift-message":     "萬惡之源銀河大帝旗下的銀河帝國宣傳局現正欲向您呈現一則(非強制的)宣傳影片，並以禮物作為回報。要觀看宣傳片以獲得禮物嗎？",
+
+            "general-watch-gift-vid":   "觀看",
+
+            "general-gift-not-watched": "銀河大帝提醒您，只有尊重影片製作者、觀看完整片段的觀眾才值得被給予禮物。",
+
+            "general-reward-complete":  "銀河帝國宣傳局感謝您的合作。",
+
+            "general-ticker-warn-head": "新聞欄即時貿易警告",
+
+            "general-ticker-warn-info": "請留意，在新聞欄點擊報價項目將會立即成交該所選貿易(如果是在您的可負擔範圍以內)。只會警告一次。",
+
+            "general-mass-message":     "聯邦公告",
+
+            "general-mass-msg-desc":    "向所有成員傳送統一訊息。",
+
+            "zolargcomm-desc":          "這是索拉格帝國的近期新科技--超長程銀河級訊息傳輸技術！",
+
+            "altarofcommerce-description":"貿易際壇是蟲族祟拜貿易之神及造就各種貿易的地方。",
+
+            "sugarcane-description":    "甜甜的甘蔗~誰不喜歡~",
+
+            "enc-resources-desc":       "查看有關遊戲內各種資源的資訊。",
+
+            "enc-tech-header":          "科技",
+
+            "enc-tech-desc":            "查看有關各種可研發的科技，以及它們的研究需求。",
+
+            "general-buildings":        "建築",
+
+            "enc-buildings-desc":       "查看有關各種可建造的建築，以及它們的建造需求。",
+
+            "general-vehicles":         "工作單,位",
+
+            "enc-vehicles-desc":        "查看有關各種工作車輛及工蟲的能力及生產成本。",
+
+            "enc-online-header":        "網上版百科全書",
+
+            "enc-online-desc":          "可互動的網上百科全書。"
