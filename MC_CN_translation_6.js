92fec5a2902c3cf52d76dcb6bf25ea0974d5cb6a
Euxcbsks <56812006+Euxcbsks@users.noreply.github.com>
2020-06-01 06:13:26 +0800

    Create MC_CN_translation_6.js

---
+++ MC_CN_translation_6.js
@@ -0,0 +1,247 @@
+            "deepdrilling-description": "An offshore platform for extracting pure Crude Oil from the depths of the sea!",
+
+            "coralradiator-description":"This small platform exposes naturally forming sea coral with extreme doses of radiation, breaking it down into trace amounts of Ore, Gold, and Aluminum.",
+
+            "offshoreslum-description": "In an ongoing effort to combat the issue of extreme Reptilian homelessness, throw an old rusted out Galactic Freight container off shore and resettle the homeless there, so you do not have to worry about the issue of extreme Reptilian homelessness.",
+
+            "dracarus-description":     "The Dracarus Mint is where Draconian gold coins are minted.  Although they are guaranteed by the Reptilians to be .999 fine Gold, they are actually made mostly of Ore.",
+
+            "diamondextractor-description":"A handy drone that can extract raw Diamonds from the ground."
+
+            "basiccrystalfurnace-description":"Just a cheap low-yield reactor for when you are desperate for power and low on resources.",
+
+            "suppertimearena-description":"The Rum and Food flow while spectators from all over the galaxy watch as a giant hungry plant devours helpless insectoids.",
+
+            "turbomicroreactor-description":"This reactor utilizes Helium 3 to turbo charge the output of the standard Advanced Microreactor.",
+
+            "primativeroboticslab-description":"Although basic Robots are considered by Reptilians to be a childs toy, there are still some instances where they are useful in industry, so it's good to be able to produce them from time to time.",
+
+            "advchiptransporter-description":"Utilizing a high gain antenna, robots, and more power, you can steal way more Microchips from across the galaxy with this advanced transporter unit.",
+
+            "autoswnode-description":   "It may look like just a large hockey puck connected to a WiFi antenna, but there is actually a robot in there rapidly churning out software.",
+
+            "antradiator-description":  "A robot-operated plate that uses massive amounts of radiation to create Triantanium, but sometimes the process fails and creates steel.  Even though the process is done out in the open, Reptilian authorities promise that there is no health concern with the radiation...",
+
+            "uhfnode-description":      "When you need serious bandwidth, you need to start building these large Ultra High Frequency nodes.",
+
+            "nimatsilo-description":    "Built with rare materials and consuming Robots, Ether, Software and Uranium during operation, this is an expensive storage unit to built and maintain, but it will give you just about all of the storage you will ever need."
+
+            "river-template":           "River Template",
+
+            "river-template-desc":      "This map type includes both land and river tiles.  Do you want the system to generate the river terrain for you, or would up like to import a template image?  A template image is a 250x250px black and white image (suggested), with white representing land and black representing river tiles.",
+
+            "river-template-auto":      "Automatic",
+
+            "river-template-image":     "Image File",
+
+            "unexplored-territory":     "Unexplored Territory",
+
+            "mail-not-available":       "No Longer Available",
+
+            "mail-not-available-info":  "The message you selected is no longer available on the server.",
+
+            "save-switch-city-ask":     "Do you want to switch to city: ",
+
+            "gen-switch":               "Switch",
+
+            "etherrover-description":   "A basic rover built for collecting and transporting Ether.",
+
+            "etherstorage-description": "When you need to store Ether on the cheap, just set it out on the sidewalk.",
+
+            "gen-utilities":            "Utilities",
+
+            "select-disp-mode":         "Select Display Mode",
+
+            "gen-default":              "Default",
+
+            "disp-trans":               "Semi-Transparent",
+
+            "disp-bld-colors":          "Building Colors Only",
+
+            "disp-ent-coverage":        "Entertainment Coverage Heatmap",
+
+            "disp-med-coverage":        "Medical Coverage Heatmap",
+
+            "disp-edu-coverage":        "Education Coverage Heatmap",
+
+            "disp-cri-coverage":        "Crime Heatmap",
+
+            "disp-lnv-coverage":        "Land Value Heatmap",
+
+            "intclinic-description":    "A more advanced small clinic, using AI and network technology to get patients in and out in just a few hours!",
+
+            "gen-tot-births":           "總累積出生人口",
+
+            "gen-age-distro":           "人口年齡長條圖",
+
+            "prod-cap-heatmap":         "Production Capacity Heatmap",
+
+            "total-play-time":          "遊玩的總時間",
+
+            "colony-age":               "殖民地的壽命 (遊戲的時間)",
+
+            "gen-prop-value":           "Property Valuation",
+
+            "gen-overall-value":        "Overall Value",
+
+            "gen-entertainment-avail":  "Entertainment Access",
+
+            "gen-education-avail":      "Education Access",
+
+            "gen-medical-avail":        "Medical Access",
+
+            "gen-building-condition":   "Structural Condition",
+
+            "gen-factors":              "Factors",
+
+            "gen-commute-rating":       "Daily Commute Rating",
+
+            "gen-safety-rating":        "Safety Rating",
+
+            "gen-accessability-rating": "Accessability Rating",
+
+            "gen-workforce-rating":     "Workforce Rating",
+
+            "health-crisis-warning":    "There is an ongoing health crisis in #cityname.  Will #governorname do anything about it?",
+
+            "gen-city-leader-name":     "The Governor",
+
+            "hospital-description":     "When you need to heal a lot of sick colonists, you need a hospital!  Take care of the sick and elderly, and charge up to $500 per asprin on their way out!",
+
+            "university-description":   "A center of knowledge and research for the colony!  Train the next generation of high-tech workers.",
+
+            "pasteclinic-description":  "In this state of the art clinic, a refined Ant Paste mixture is injected into the blood of the patient, somehow causing health.",
+
+            "deport-elderly":           "Deport the Elderly",
+
+            "deport-elderly-desc":      "Kick anybody over the age of 65 out of #cityname.  They can go and enjoy their golden years somewhere else!",
+
+            "generating-terrain":       "Generating Terrain",
+
+            "generating-terrain-desc":  "Please wait while the global terrain features for the region are generated.  This could take a minute.",
+
+            "small-scr-new-reg-help":   "Welcome to your new region.  To get started, click on the Build icon to found your first city!",
+
+            "hospital2-description":    "An even larger medical facility for healing tons of sick colonists!",
+
+            "transclinic-description":  "A small medical clinic run entirely by software and robots.",
+
+            "production-priority":      "Production Priority",
+
+            "production-priority-info": "When all other factors are equal, structures with a higher Production Priority will get new workers first.",
+
+            "employment-capacity":      "Employment Capacity",
+
+            "employment-capacity-info": "Normally you would run your buildings at full capacity, but if you need to fine tune your production levels, you can set an employment capacity limit.",
+
+            "lgstadium-description":    "When you really need to pack in the spectators, build this large stadium and stuff them into the nosebleed section!",
+
+            "smsecurity-description":   "Provides basic security service and a few jail cells for a small colony.",
+
+            "secstation-description":   "A standard sized security station, providing peace of mind and an area of protection.",
+
+            "secprecincthq-description":"This large security facility can hold a lot of criminals, keeping the streets save for little bobby and little susie.",
+
+            "reeducation-description":  "When citizens commit crimes, you can reeducate them in this facility.  Ignore the brutal screams and large amounts of electricity being pumped into the building.",
+
+            "squareofshame-description":"When minor crimes are committed, the guilty insectoid must be tied up at the public Square of Shame where onlookers can laugh and spit regurgitated sugar at them.",
+
+            "moundofjustice-description":"Serve justice from atop of this mound, while criminals are locked up in the halls below.",
+
+            "colonist-rendering":       "Colonist Rendering",
+
+            "colonist-rendering-desc":  "Set the maximum number of colonists you will allow the game to render at one time.  The larger the count, the larger the performance impact.",
+
+            "pub2-description":         "This upgraded Pub will serve even more guests as well as tourists, providing not only drinks, but also Food and free WiFi!",
+
+            "nanotech-desc":            "Once you're done building giant things, grab your microscope and do a complete 180 by building things as tiny as possible using Advanced Nanotechnology!",
+
+            "nanitefactory-description":"An advanced facility for creating Nanites - microscopic AI powered robots that can do almost anything!",
+
+            "nanoclinic-description":   "Fast and easy medical care.  Just inject the patient with Nanites and let them take care of the rest!  In and out in 2 minutes.",
+
+            "naniteinjection-description":"When you are finally ready to give up on your individuality and join the collective, head to the Nanite Infusion Clinic, where you will be assimilated by having nanites permanently infused into your body.  You will be super smart.  But will you still be YOU?",
+
+            "max-payroll-assistance":   "Maximum Payroll Assistance",
+
+            "max-assist-desc":          "A hard maximum for how much money you are willing to pay a member of your commonwealth during one payroll period.",
+
+            "keyboard-payroll":         "Show Payroll Policies",
+
+            "love-gov-day":             "The people of #cityname have declared today to be 'We Love #governorname Day'!",
+
+            "love-gov-day-2":           "More parents in #cityname are choosing to name their children after #governorname."
+
+            "patentoffice-description": "Every day, reptilian businessmen are coming up with new ideas, and your government needs to be there to grab a piece of the pie!  Generates civics while stealing money and research from businesses.",
+
+            "centancstud-description":  "This facility charters, authorizes and coordinates starships to travel the galaxy and take ancient alien resources from other worlds, by any means necessary.",
+
+            "naniteden-description":    "A specialized lab where the artisan Nanite builder works around the clock to create Nanites by hand, under a high powered microscope.",
+
+            "microevaporator-description":"Using the power of Nanites, create large amounts of Helium 3 and other base elements out of raw Ore.",
+
+            "microgrower-description":  "This is what you get when you merge the power of the Gold Grower and the Aluminum Grower, mixed in with a bunch of Nanites.  Create lots of Gold and Aluminum!",
+
+            "unholyhallofancients-description":"Shrouded in secrecy, Antaura goes into this mystic hall, and a lot of expensive stuff comes out.  How does it work?  Only the mystics who work there know for sure.",
+
+            "nanitefab-description":    "An advanced lab that constructs microscopic Nanites, tiny intelligent robots.",
+
+            "nanocrystaltubes-description":"A series of microscopic crystalline-laced mine shafts where Nanites quickly extract valuable resources from deep in the ground.",
+
+            "helnanomine-description":  "A tiny mining facility operated by Nanites, yielding large amounts of raw materials!"
+
+            "gen-bookmark":             "書籤",
+
+            "gen-bookmarks":            "訂選",
+
+            "gen-unbookmark":           "取消訂選",
+
+            "backup-copy":              "備份",
+
+            "backup-copy-desc":         "創建一個備份讓你的遊戲進度可以安全保存，來避免你的殖民地資料遺失或是儲存錯誤。",
+
+            "backup-region-notice":     "Be advised that backing up an entire region may take a few minutes depending on the size of the region and it's cities.  Please do not close out of My Colony until the backup has completed.",
+
+            "gen-begin":                "開始",
+
+            "regbussing-description":   "Load up colonists from one city and transport them to neighboring cities in your region.  Allows your citizens to live and work in different cities, and even immigrate between cities in the region.",
+
+            "regbus-description":       "A hard working bus that brings hard working colonists from one city to the next!",
+
+            "publictrans-desc":         "Research and unlock the logistics involved in moving colonists efficiently around the colony.",
+
+            "gen-last-save":            "最近一次儲存",
+
+            "gen-stat":                 "名稱",
+
+            "gen-value":                "資料",
+
+            "gen-filename":             "檔案名稱",
+
+            "gen-civilization":         "隸屬文明",
+
+            "gen-play-client-ver":      "最近遊玩的版本",
+
+            "gen-first-play-date":      "首次遊玩日期",
+
+            "gen-first-play-ver":       "首次遊玩的版本",
+
+            "gen-online-game":          "線上模式",
+
+            "gen-offline-game":         "離線模式",
+
+            "gen-game-mode":            "遊戲模式",
+
+            "gen-game-owner":           "遊戲創建者",
+
+            "gen-file-size":            "檔案大小",
+
+            "gen-game-type":            "遊戲類型",
+
+            "gen-planet-type":          "星球類型",
+
+            "gen-def-race":             "隸屬種族",
+
+            "gen-assoc-files":          "有關聯的檔案"
+
+        },
